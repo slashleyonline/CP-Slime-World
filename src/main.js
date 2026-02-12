587bce24f6d19021a6a0bb88411e1c99db7fb5de
NonDrip <dseward@ucsc.edu>
2026-02-11 23:25:40 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: Slime World
+// Name: Ashley Seward
+// Date: 2/11/2026
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
